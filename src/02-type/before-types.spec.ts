119831c7b9b0e77317bcb1f067be96f8835607f5
Greg Murray <greg.murray@volusion.com>
2019-03-05 13:29:38 -0600

    add chapter 4 exercise

--- src/02-type/before-types.spec.ts
+++ src/02-type/before-types.spec.ts
@@ -10,13 +10,13 @@ let bankAccount = {
 };
 
 describe("Module 2 before exercise", () => {
-  it("should deposit money", () => {
-    let myself = {
-        name: "Max",
-        bankAccount: bankAccount,
-        hobbies: ["Sports", "Cooking"]
-    };
-    myself.bankAccount.deposit(3000);
-    expect(myself.bankAccount.money).toEqual(5000);
+    it("should deposit money", () => {
+        let myself = {
+            name: "Max",
+            bankAccount: bankAccount,
+            hobbies: ["Sports", "Cooking"]
+        };
+        myself.bankAccount.deposit(3000);
+        expect(myself.bankAccount.money).toEqual(5000);
     });
 });
---
+++ src/04-ts-and-es6/before-es5.spec.ts
@@ -0,0 +1,60 @@
+// exercise: convert to types and interfaces
+
+/* tslint:disable */
+describe("Convert to es6 and ts", () => {
+    // @ts-ignore
+    var double = function (value) {
+        return value * 2;
+    };
+    // @ts-ignore
+    var greet = function (name) {
+        if (name === undefined) {
+            name = "Max";
+        }
+        return "Hello, " + name;
+    }
+    // @ts-ignore
+    var mathMin = function (numbers) {
+        return Math.min.apply(Math, numbers);
+    }
+    // @ts-ignore
+    var addToArray = function (ary1, ary2) {
+        Array.prototype.push.apply(ary1, ary2);
+        return ary1;
+    }
+    it("should double result", () => {
+        expect(double(10)).toEqual(20);
+    });
+    it("should greet name passed in", () => {
+        expect(
+            greet("Homer")
+        ).toEqual("Hello, Homer");
+    });
+    it("should greet Max when no parameter", () => {
+        // @ts-ignore
+        expect(greet()).toEqual("Hello, Max");
+    });
+    const numbers = [-3, 33, 38, 5];
+    it("should return min value of list", () => {
+        expect(mathMin(numbers)).toEqual(-3);
+    });
+    it("should add to array", () => {
+        expect(addToArray([55,20], numbers)).toEqual([55,20,-3, 33, 38, 5]);
+    });
+    it("should create variables from an array", () => {
+        var testResults = [3.89, 2.99, 1.38];
+        var result1 = testResults[0];
+        var result2 = testResults[1];
+        var result3 = testResults[2];
+        var obj = {
+            r1: result1,
+            r2: result2,
+            r3: result3
+        };
+        expect(obj).toEqual({
+            r1: 3.89,
+            r2: 2.99,
+            r3: 1.38
+        });
+    });
+});
